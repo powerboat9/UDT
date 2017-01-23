ff41130d287fbbb42f0f2becc39a2c3a87384836
powerboat9 <powerboat9.gamer@gmail.com>
2017-01-23 16:19:24 -0500

    Did some work on transmiting

---
+++ transmit.c
@@ -0,0 +1,3 @@
+#include "include/sound.h"
+
+void transmitPacket(
--- transmit.py
+++ transmit.py
@@ -2,3 +2,12 @@
 
 inp = base64.b64encode("Hi")
 charList = list((string.ascii_letters + string.digits + "+/= ").replace("~", "") + "~")
+
+class WireIn():
+    def __init__(self, channel, baud = 5, 
+volPercent = 100)
+        self.channel = channel
+        self.baud = baud
+        self.volPercent = volPercent
+        self.buffer = []
+    def transmit
