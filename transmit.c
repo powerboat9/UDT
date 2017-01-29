38f70a86261143b853f69cb4b396791d9a4042fc
powerboat9 <powerboat9@users.noreply.github.com>
2017-01-29 16:38:30 -0500

    Did some work

--- transmit.c
+++ transmit.c
@@ -1,3 +1,31 @@
-#include "include/sound.h"
+#include "include/sound.c"
+#define __BAUD_ID_PADDING__ 8
 
-void transmitPacket(
+struct Wire {
+    unsigned long baud;
+    unsigned short baseFreq;
+    unsigned short binSize;
+    unsigned char binNum;
+};
+
+struct Packet {
+  /*unsigned char baud[__BAUD_ID_PADDING__];*/
+    unsigned long to;
+    unsigned long from;
+    unsigned short dataSize;
+    unsigned char *data;
+    unsigned char more;
+};
+
+void transmitData(struct Wire wire, struct Packet packet) {
+    initPlay();
+    for (int i = 0; i < __BAUD_ID_PADDING__; i++) {
+        if ((i % 2) == 0) {
+            playTone(wire.baseFreq, (double)(1 / wire.baud));
+        } else {
+            wait((double)(1 / wire.baud));
+        }
+    }
+    char bitBufferSize;
+    char bitBuffer;
+    for i
